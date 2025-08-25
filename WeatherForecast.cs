89d8f42cfe002da4fa86f2e31c898981240882da
Hepatica <danil.tkachenko@getmore.cz>
2025-08-25 23:34:38 +0200

    Add project files.

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Microsoft_.NET_OCR_CV_Accelerate_Efficiency_and_Innovation
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
