fb19dc1758ffd997c08c75815dd70d5301a81abb
Hepatica <danil.tkachenko@getmore.cz>
2025-08-26 06:38:02 +0200

    Start implementing DocumentIntelligenceController

---
+++ Controllers/DocumentIntelligenceController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Microsoft_.NET_OCR_CV_Accelerate_Efficiency_and_Innovation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DocumentIntelligenceController : ControllerBase
+    {
+        public DocumentIntelligenceController()
+        {
+        }
+
+        [HttpGet(Name = "GetDocumentIntelligence")]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                Test = "test"
+            });
+        }
+    }
+}
